f25e84f7cc9981bbc702adf68ec86bb6fa26d308
奉伟 <landwind1180@gmail.com>
2020-08-12 15:07:37 +0800

    update db.js

--- db.js
+++ db.js
@@ -2,10 +2,14 @@ const mongoose = require("mongoose");
 const { Book, BookCategory } = require("./models");
 
 mongoose
-  .connect("mongodb://LandWind:27017/cat-shop", {
-    // useNewUrlParser: true
-    // useUnifiedTopology:true
-    useUnifiedTopology: true, useNewUrlParser: true, useFindAndModify: false
+  // .connect("mongodb://LandWind:27170/cat-shop", {
+      .connect("mongodb://localhost:27017/cat-shop", {
+    // useNewUrlParser: true,
+    // useUnifiedTopology:true,
+    useUnifiedTopology: true, 
+    useNewUrlParser: true,
+     useFindAndModify: false,
+     serverSelectionTimeoutMS: 10000
   })
   .then(res => {
     // console.log(res);
